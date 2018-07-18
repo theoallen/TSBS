c01fa0bd03be7cad0c2c2c02f28384e90ead15a0
theoallen <theoallen@users.noreply.github.com>
2018-07-18 16:06:56 +0700

    Update Basic Module.rb

--- Grid Battle System/Basic Module.rb
+++ Grid Battle System/Basic Module.rb
@@ -1,7 +1,7 @@
 #===============================================================================
-# TSBS Addon - Grid Battle System (Pre-Alpha)
+# TSBS Addon - Grid Battle System (Alpha)
 #-------------------------------------------------------------------------------
-# REQUIREMENTS :
+# DEPENDENCIES (no plan to remove the dependencies at the moment) :
 # - YEA Battle Engine
 # - YEA Free Turn Battle
 # - Theolized SBS
@@ -20,20 +20,45 @@ module Grid
   #============================================================================
     Position =  [ # <-- Don't touch this
   #----------------------------------------------------------------------------
-  # Set position here
+  # Set position here. If you are using 3x6 grid, then you need to format it
+  # this way
+  #  X ------------------------------------> 6 Columns
+  #  | [x,y],[x,y],[x,y],[x,y],[x,y],[x,y],
+  #  | [x,y],[x,y],[x,y],[x,y],[x,y],[x,y],
+  #  | [x,y],[x,y],[x,y],[x,y],[x,y],[x,y],
+  #  V
+  # 3 Rows
   #----------------------------------------------------------------------------
 [88,170],[140,170],[192,170],[246,170],[300,170],[352,170],[406,170],[460,170],
 [77,206],[132,206],[188,206],[244,206],[302,206],[356,206],[414,206],[471,206],
 [55,236],[114,236],[180,236],[240,236],[306,236],[364,236],[430,236],[493,236],
 ]
   #============================================================================
-  
-  EnemySpaces = [0,1,2,3,8,9,10,11,16,17,18,19]
-  ActorSpaces = [4,5,6,7,12,13,14,15,19,20,21,22,23]
-  
-  # DON'T TOUCH
-  Movement = [1,2,3,4,6,7,8,9]
-  
+    EnemySpaces = [0,1,2,3,8,9,10,11,16,17,18,19]
+    ActorSpaces = [4,5,6,7,12,13,14,15,19,20,21,22,23]
+  #----------------------------------------------------------------------------
+  # Each grid has index. Started from top left being index 0
+  # If you're using 3x6, the grid index is going to be like this
+  #
+  #  X -------------------------------> 6 Columns
+  #  | [ 0],[ 1],[ 2],[ 3],[ 4],[ 5],
+  #  | [ 6],[ 7],[ 8],[ 9],[10],[11],
+  #  | [12],[13],[14],[15],[16],[17],
+  #  V
+  # 3 Rows
+  #
+  # Means if you set EnemySpaces to = [0,1,2,6,7,8,12,13,14]
+  # They will only move on that area
+  #
+  # Or you can try to show the index by setting ShowIndex below to true.
+  # Remember that it needs to set to false in final game deployment.
+  #----------------------------------------------------------------------------
+    ShowIndex = true
+  #============================================================================
+  # DON'T TOUCH THIS
+  #----------------------------------------------------------------------------
+    Movement = [1,2,3,4,6,7,8,9]
+  #============================================================================
 end
 
 #===============================================================================
@@ -51,18 +76,20 @@ TSBS::AnimLoop.merge!(
   }
 )
 #===============================================================================
-# * Grid Counting Module
+# End of basic module editable area
+#===============================================================================
+# * Grid Module
 #===============================================================================
 
 class << Grid
   #----------------------------------------------------------------------------
   # * Grid direction rules
   #----------------------------------------------------------------------------
-  # 2 = DOWNWARD
-  # 4 = FORWARD
-  # 6 = BACKWARD
-  # 8 = UPWARD
-  #
+  # 2 = DOWN
+  # 4 = LEFT
+  # 6 = RIGHT
+  # 8 = UP
+  
   # 1 = DOWN-LEFT
   # 3 = DOWN-RIGHT
   # 7 = UP-LEFT
@@ -293,3 +320,76 @@ elsif Grid::Position.any? {|arr| arr.size < 2}
   exit
 end
 #-------------------------------------------------------------------------------
+#===============================================================================
+# Display grid index
+#===============================================================================
+if Grid::ShowIndex
+
+class Grid_Debug < Sprite
+  #----------------------------------------------------------------------------
+  # * Initialize
+  #----------------------------------------------------------------------------
+  def initialize(viewport, index)
+    super(viewport)
+    @index = index
+    self.bitmap = Bitmap.new(32,32)
+    self.bitmap.entire_fill(Color.new(255,255,255,32))
+    self.bitmap.border_fill(Color.new(255,255,255,255))
+    self.bitmap.draw_text(bitmap.rect, index, 1)
+    self.x = Grid::Position[index][0]
+    self.y = Grid::Position[index][1]
+    self.z = 4
+    self.ox = self.oy = 16
+  end
+  
+end
+
+class Spriteset_Grid_Debug
+  
+  def initialize(vport)
+    @grids = []
+    Grid.max_index.times do |i|
+      @grids << Grid_Debug.new(vport, i)
+    end
+  end
+  
+  def update
+    @grids.each {|g| g.update }
+  end
+  
+  def dispose
+    @grids.each {|g| g.dispose }
+  end
+  
+end
+  
+class Spriteset_Battle
+
+  #----------------------------------------------------------------------------
+  # * Initialize
+  #----------------------------------------------------------------------------
+  alias tsbs_grid_debug_init initialize
+  def initialize
+    tsbs_grid_debug_init
+    @debug_grid = Spriteset_Grid_Debug.new(@viewport1)
+  end
+  #----------------------------------------------------------------------------
+  # * Update
+  #----------------------------------------------------------------------------
+  alias tsbs_grid_debug_update update
+  def update
+    tsbs_grid_debug_update
+    @debug_grid.update if @debug_grid
+  end
+  #----------------------------------------------------------------------------
+  # * Dispose
+  #----------------------------------------------------------------------------
+  alias tsbs_grid_debug_dispose dispose
+  def dispose
+    tsbs_grid_debug_dispose
+    @debug_grid.dispose
+  end
+  
+end
+
+end
